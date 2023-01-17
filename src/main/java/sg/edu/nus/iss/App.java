7fba9a1a9ec075d0f30c12a35ee72217ef2fbe02
Yongli <yl.low91@gmail.com>
2023-01-17 11:31:23 +0800

    added different threadings

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,8 @@
 package sg.edu.nus.iss;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Hello world!
  */
@@ -12,22 +15,63 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 5; i++) {
-                    System.out.println(Thread.currentThread().getName() + "\tRunnable ..." + i);
-                }
-            }
-        });
+        // Thread thread1 = new Thread(new Runnable() {
+        //     @Override
+        //     public void run() {
+        //         for (int i = 0; i < 5; i++) {
+        //             System.out.println(Thread.currentThread().getName() + "\tRunnable ..." + i);
+        //         }
+        //     }
+        // });
 
-        thread1.start();
+        // thread1.start();
 
-        MyRunnableImplementation mRI = new MyRunnableImplementation();
-        Thread thread2 = new Thread(mRI);
-        thread2.start();
+        MyRunnableImplementation mRI1 = new MyRunnableImplementation("Task 1");
+        MyRunnableImplementation mRI2 = new MyRunnableImplementation("Task 2");
+        MyRunnableImplementation mRI3 = new MyRunnableImplementation("Task 3");
+        MyRunnableImplementation mRI4 = new MyRunnableImplementation("Task 4");
+        MyRunnableImplementation mRI5 = new MyRunnableImplementation("Task 5");
+        // Thread thread2 = new Thread(mRI);
+        // thread2.start();
 
-        Thread thread3 = new Thread(mRI);
-        thread3.start();
-    }
+        // Thread thread3 = new Thread(mRI);
+        // thread3.start();
+
+        // Single Thread, can only execute one task before executing another
+
+        // ExecutorService executorService = Executors.newSingleThreadExecutor();
+        // executorService.execute(mRI1);
+        // executorService.execute(mRI2);
+
+        // pool-1-thread-1 Task 1   Runnable...0
+        // pool-1-thread-1 Task 1   Runnable...1
+        // pool-1-thread-1 Task 1   Runnable...2
+        // pool-1-thread-1 Task 1   Runnable...3
+        // pool-1-thread-1 Task 1   Runnable...4
+        // pool-1-thread-1 Task 2   Runnable...0
+        // pool-1-thread-1 Task 2   Runnable...1
+        // pool-1-thread-1 Task 2   Runnable...2
+        // pool-1-thread-1 Task 2   Runnable...3
+        // pool-1-thread-1 Task 2   Runnable...4
+
+        // executorService.shutdown();
+
+        // ExecutorService executorService = Executors.newFixedThreadPool(3);
+        // executorService.execute(mRI1);
+        // executorService.execute(mRI2);
+        // executorService.execute(mRI3);
+        // executorService.execute(mRI4);
+        // executorService.execute(mRI5);
+        // executorService.shutdown();
+
+        // Never specify any thread number but cachedthreadpool will determine how many threads it will use
+        // The limit is how many threads your CPU has.
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(mRI1);
+        executorService.execute(mRI2);
+        executorService.execute(mRI3);
+        executorService.execute(mRI4);
+        executorService.execute(mRI5);
+        executorService.shutdown();
+}
 }
